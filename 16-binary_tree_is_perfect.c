a7aa716d3da643bd915f2db1864ce2694f088551
mena5800 <mr.safwat55@gmail.com>
2023-08-31 18:42:55 +0300

    fix 16 - 18

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -51,7 +51,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+		return (1);
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -20,8 +20,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left == node)
-		return (node->parent->parent->left);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
 
-	return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
